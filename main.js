b2da10f83d6d12cbbd88a967b241b791900011c8
uguribrahimov <uguribrahimov04@gmail.com>
2024-05-23 14:00:47 +0400

    .

---
+++ main.js
@@ -0,0 +1,119 @@
+// let day = prompt("Haftanın gününü giriniz");
+
+// if (day === "1") {
+//     alert("Bazar ertəsi");
+// }
+// else if (day === "2") {
+//     alert("Çərşənbə axşamı");
+// }
+// else if (day === "3") {
+//     alert("Çərşənbə");
+// }
+// else if (day === "4") {
+//     alert("Cümə axşamı");
+// }
+// else if (day === "5") {
+//     alert("Cümə");
+// }
+// else if (day === "6") {
+//     alert("Şənbə");
+// }
+// else if (day === "7") {
+//     alert("Bazar");
+// }
+// else {
+//     alert("Belə bir gün yoxdur");
+// }                                                       heftenin gunleri
+
+
+
+// let day = confirm("Bu gunun hava praqnozunu gormek isteyirsiz?")
+
+// if (day===true) {
+//     alert("Hava bugun guneslidir")
+// }
+// else {
+//     alert("gostermirem")
+// }                                                                  hava   
+
+
+// let age = prompt("Yasinizi daxil edin");
+// let work = confirm("isiniz var?");
+
+// if (age>50 && !work) {
+//     alert("Tam teqaud qazandiniz")
+// }
+// else if (age == 50 && !work) {
+//     alert("50% teqaud qazandiniz")
+// }
+// else {
+//     alert("Teqaud qazana bilmediniz")
+// }                                                      teqaud
+
+
+
+
+
+// let number = prompt("Bir reqem yazin:")
+
+// if (!isNaN(number)) {
+//     let kok = Math.sqrt(number);
+//     alert("reqemin kokalti:  " + kok)
+// }
+// else {
+//     alert(reqem deyil)
+// }                                          kokalti
+
+
+
+let number = prompt("Balinizi qeyd edin")
+
+if (number>=91 && number<=100) {
+    alert("A aldiniz ve imtahandan ugurla kecdiniz")
+}
+
+else if (number>=81 && number<=90) {
+    alert("B aldiniz ve Novbeti merheleye buraxildiniz")
+}
+else if (number>=71 && number<=80) {
+    alert("C aldiniz ve Yeniden muraciet edin")
+}
+else if (number>=61 && number<=70) {
+    alert("D aldiniz ve Kesildiniz")
+}
+else {
+    alert("duzgun melumat deyil")
+}
+
+
+
+
+// let dersSaati= prompt("Ayliq Ders saatini daxil edin")
+// let saatBasiOdenis= prompt("Saat basi odenisi daxil edin")
+// let maas = dersSaati* saatBasiOdenis
+
+// if (dersSaati > 150) { 
+// let artimFaizi = 0.1
+// maas += maas*artimFaizi
+
+// }
+
+// alert (maas)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
